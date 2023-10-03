172559ef623068ae503eb6e5a931f3f82f70c438
Hjoon <shj83811081@gmail.com>
2023-10-04 02:25:21 +0900

    intput check and function prototype

--- PA1.c
+++ PA1.c
@@ -2,15 +2,15 @@
 #include <string.h>
 
 
-void insert(int element) {} // Inserts an integer element into the priority queue. 
+void insert(int element); // Inserts an integer element into the priority queue. 
 
-int delete_min() {} // Deletes and returns the minimum element.
-int delete_max() {} // Deletes and returns the maximum element.
-int delete_median() {} // Deletes and returns the median element.
+int delete_min(); // Deletes and returns the minimum element.
+int delete_max(); // Deletes and returns the maximum element.
+int delete_median(); // Deletes and returns the median element.
 
-int find_min() {} // Fetches but does not remove the minimum element. 
-int find_max() {} // Fetches but does not remove the maximum element. 
-int find_median() {} // Fetches but does not remove the median element.
+int find_min(); // Fetches but does not remove the minimum element. 
+int find_max(); // Fetches but does not remove the maximum element. 
+int find_median(); // Fetches but does not remove the median element.
 
 
 
@@ -24,17 +24,18 @@ int main() {
     char value_type;
     int element;
 
-     scanf("%d", &operation_num);
+    scanf("%d", &operation_num);
 
     while (operation_num--)
     {
         // Get input from user and execute operations.
         scanf(" %c %s", &operation_type, operation_target);
-
+        
         if (operation_type == 'I') 
         {
             sscanf(operation_target, " %d", &element);
-            insert(element);
+            //insert(element);
+            printf("%c %d\n",operation_type, element);
         }
 
         else 
@@ -44,21 +45,39 @@ int main() {
             if (operation_type == 'D')
             {
                 if (value_type == 'M') 
-                { delete_min; }
+                { 
+                    //delete_min; 
+                    printf("%c %c\n",operation_type, value_type);
+                }
                 else if (value_type == 'X') 
-                { delete_max; }
+                { 
+                    //delete_max; 
+                    printf("%c %c\n",operation_type, value_type);
+                }
                 else if (value_type == 'E')
-                { delete_median; }
+                {
+                    //delete_median; 
+                    printf("%c %c\n",operation_type, value_type);
+                }
             }
 
             else if (operation_type == 'F')
             {
                 if (value_type == 'M') 
-                { find_min; }
+                { 
+                    //find_min; 
+                    printf("%c %c\n",operation_type, value_type);
+                }
                 else if (value_type == 'X') 
-                { find_max; }
+                { 
+                    //find_max; 
+                    printf("%c %c\n",operation_type, value_type);
+                }
                 else if (value_type == 'E')
-                { find_median; }
+                { 
+                    //find_median; 
+                    printf("%c %c\n",operation_type, value_type);
+                }
             }
         }
     }
